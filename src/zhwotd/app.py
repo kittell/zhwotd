207cbcc4af9e668a062503c012e720b2a6a703d2
kittell <kirk.kittell@gmail.com>
2025-12-27 15:32:29 -0600

    starting db setup procedures in DatabaseManager

--- src/zhwotd/app.py
+++ src/zhwotd/app.py
@@ -1,7 +1,11 @@
 import json
 import os
+from pathlib import Path
+import sqlite3
+from sqlalchemy import create_engine, text
 from zhwotd.gui import GUI
-from zhwotd.zhwotd import DatabaseManager
+
+
 
 class Application:
     def __init__(self):
@@ -19,5 +23,66 @@ class Application:
         return config
 
     def start(self):
+        self.dbm.connect()
         gui = GUI(self, self.config)
+        return
+    
+
+class DatabaseManager:
+    """Handle operations involving the entire database and its structure
+    """
+    def __init__(self, config):
+        self.config = config
+        return
+    
+    def connect(self):
+        db_type = self.config['database']['type']
+        db_name = self.config['database']['name']
+        script_dir = Path(__file__).resolve().parent
+        db_path = script_dir / db_name
+        self.db_url = db_type + ':///' + str(db_path)
+
+        self.engine = create_engine(self.db_url)
+        if self.db_url.is_file() == False:
+            self.create_new_db(self.db_url)
+
+        return
+
+    def create_new_db(self):
+        
+        schema = self._load_schema(self.config['schema_path'], self.config['use_version'])
+        with self.engine.connect() as conn:
+            # Creates database file if it doesn't exist
+            for table_name, table_def in schema['tables'].items():
+                columns_sql = []
+
+                for col_name, col_def in table_def['columns'].items():
+                    col_parts = [col_name, col_def['type']]
+                    
+                    if col_def.get('primary_key'):
+                        col_parts.append('PRIMARY KEY')
+                    # TODO: restart from here
+
+
+        return
+    
+    def _load_schema(self, schema_path: str, version: int):
+        path = Path(schema_path).resolve()
+        with path.open('r', encoding='utf-8') as f:
+            data = json.load(f)
+        
+        if str(version) in data['versions']:
+            schema = data['versions'][str(version)]
+        
+        # TODO: else condition
+
+        return schema
+    
+    
+    def backup_db(self):
+        # TODO: create copy of db file with current date time appended
+        return
+    
+    def modify_db(self):
+        # TODO: fuzzy... how to make changes to db structure from within program
         return
--- src/zhwotd/zhwotd.py
+++ src/zhwotd/zhwotd.py
@@ -1,34 +1,3 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-
-class DatabaseManager:
-    """Handle operations involving the entire database and its structure
-    """
-    def __init__(self, config):
-        return
-    
-    def configure_engine(self):
-        db_type = self.config['database']['type']
-        db_name = self.config['database']['name']
-        db_url = db_type + ':///' + db_name
-        engine = create_engine(db_url, echo=False)
-        return engine
-
-    def create_db(self):
-        # TODO: to be called from GUI admin page or command line
-        # TODO: read schema from json file; consider how to manage different schema versions
-        # TODO: provide popup window with text box and keywords if doesn't exist
-        return
-    
-    def backup_db(self):
-        # TODO: create copy of db file with current date time appended
-        return
-    
-    def modify_db(self):
-        # TODO: fuzzy... how to make changes to db structure from within program
-        return
-
 class QueryBuilder:
     """Build queries specific to program function, then handoff to DatabaseManager
     """
